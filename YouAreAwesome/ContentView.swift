4da5573a1b965a7e0977554a5973e0bbfc1eef87
realnotben <benjaminfigueroa022@gmail.com>
2025-11-19 11:31:34 -0800

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by FIGUEROA, BENJAMIN on 11/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+    @State private var imageNumber = 0
+    @State private var messageNumber = 0
+    var body: some View {
+        
+        VStack {
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            Button("Show Message") {
+                let messages = ["You Are Awesome!",
+                                "You Are Great!",
+                                "You Are Fantastic!",
+                                "Fabulous? That's You!",
+                                "You Make Me Smile!",
+                                "When the Genius Bar Needs Help, They'll Call You!"]
+                
+                message = messages[messageNumber]
+                messageNumber += 1
+                if messageNumber == messages.count {
+                    messageNumber = 0
+                }
+                
+                imageName = "image\(imageNumber)"
+                //imageNumber = imageNumber + 1
+                imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .padding()
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by FIGUEROA, BENJAMIN on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
